882e3243510f69f0b7c42d486fa42b38966c008f
leirA123 <100305573+leirA123@users.noreply.github.com>
2022-11-27 03:08:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(
   })
 )  
 
-app.get("/log?format=json&hasfast=true&authuser=0", (req, res) => {
+app.get("/_/GoogleFinanceUi/data/batchexecute?rpcids=Ba1tad&source-path=%2Fleira123.github.io-ibo1%2F&f.sid=3848989295377688160&bl=boq_finance-ui_20221004.02_p0&hl=pt-BR&soc-app=162&soc-platform=1&soc-device=1&_reqid=1611076&rt=c", (req, res) => {
   res.json({ })
 })
 

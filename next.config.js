e4a857c2946162dbaa8248dff368dd067374775e
leirA123 <100305573+leirA123@users.noreply.github.com>
2022-11-27 01:45:19 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -6,7 +6,7 @@ module.exports = {
                source: '/:path*',
                headers: [
                     { key:'Access-Control-Allow-Credentials', value: 'true' },
-                    { key:'Access-Control-Allow-Origin', value: 'http://play.google.com' },
+                    { key:'Access-Control-Allow-Origin', value: 'https://www.google.com/log?format=json&hasfast=true&authuser=0' },
                     { key:'Access-Control-Allow-Methods', value: 'GET, POST, OPTIONS, PUT, DELETE, HEAD' }
                     { key:'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, Authorization }
                ],
